2e6b2bfdbae9be2b9ef0913472c3b9d9be0d5f3d
Bijoy-karmokar <bijoykarmokar320@gmail.com>
2026-02-11 19:55:29 +0600

    spinner

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,17 @@
+const createElements = (arr)=>{
+    const htmlElements = arr.map(el=>`<span class="btn">${el}</span>`);
+    return htmlElements.join(" ");
+}
+
+const manageSpinner =(status)=>{
+    if(status == true){
+        document.getElementById("spinner").classList.remove("hidden");
+        document.getElementById("word-container").classList.add('hidden');
+    }else{
+         document.getElementById("word-container").classList.remove("hidden");
+        document.getElementById("spinner").classList.add('hidden');
+    }
+}
 const loadLessons = ()=>{
     fetch('https://openapi.programming-hero.com/api/levels/all')
     .then(res=>res.json())
@@ -9,6 +23,7 @@ const removeActive=()=>{
       lessonsButton.forEach((btn)=>btn.classList.remove("active"));
 }
 const loadLevelWord=(id)=>{
+    manageSpinner(true);
     // console.log(id);
     fetch(`https://openapi.programming-hero.com/api/level/${id}`)
     .then(res=>res.json())
@@ -19,7 +34,56 @@ const loadLevelWord=(id)=>{
         displayWords(data.data)
     }
     )
-    
+}
+
+// {
+//     "word": "Diligent",
+//     "meaning": "পরিশ্রমী",
+//     "pronunciation": "ডিলিজেন্ট",
+//     "level": 5,
+//     "sentence": "He is a diligent student who studies every day.",
+//     "points": 5,
+//     "partsOfSpeech": "adjective",
+//     "synonyms": [
+//         "hardworking",
+//         "industrious",
+//         "persistent"
+//     ],
+//     "id": 4
+// }
+
+const loadWordDetail=async(id)=>{
+    //    console.log(id);
+    const url = `https://openapi.programming-hero.com/api/word/${id}`;
+    const res = await fetch(url);
+    const details = await res.json();
+    displayWordDetail(details.data);
+}
+
+const displayWordDetail =(word)=>{
+    // console.log(word);
+    const wordDetails = document.getElementById("word-container");
+    wordDetails.innerHTML=`
+            <div class="text-2xl font-bold">
+                   ${word.word} (<i class="fa-solid fa-microphone-lines"></i>:ইগার)
+                 </div>
+                 <div>
+                   <h3 class="font-semibold">Meaning</h3>
+                   <p>${word.meaning}</p>
+                 </div>
+                 <div>
+                   <h3 class="font-semibold">Example</h3>
+                   <p>${word.sentence}</p>
+                 </div>
+                 <div>
+                    <p>সমার্থক শব্দ গুলো</p>
+                    <div>
+                      ${createElements(word.synonyms)}
+                    </div>
+                 </div>
+    `;
+
+    document.getElementById('word_details').showModal();
 }
 const displayWords = (words)=>{
     // console.log(words);
@@ -34,6 +98,7 @@ const displayWords = (words)=>{
                     <h2 class="font-medium text-4xl">নেক্সট Lesson এ যান</h2>
             </div>
         `
+        manageSpinner(false)
         return;
     }
     words.forEach(word=>{
@@ -50,14 +115,14 @@ const displayWords = (words)=>{
                     <p class="text-lg font-medium">Meaning /Pronounciation</p>
                     <div class="text-2xl font-hind font-semibold">"${word.meaning ? word.meaning : 'শব্দটির অর্থ পাওয়া যায়নি'} / ${word.pronunciation ? word.pronunciation : 'উচ্চারণ পাওয়া যায়নি'}"</div>
                     <div class="flex justify-between items-center mt-13">
-                        <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+                        <button onclick='loadWordDetail(${word.id})' class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
                         <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-low"></i></button>
                     </div>
                 </div>
         `
         wordContainer.append(createCard);
     })
-    
+    manageSpinner(false);
 }
 const displayLesson =(lessons)=>{
     // console.log(lessons);
